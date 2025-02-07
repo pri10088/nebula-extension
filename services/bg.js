35cde8d24f490c77caeafab21fd2612bd4aab7ee
pri10088 <shanmugapriyaar0@gmail.com>
2025-02-07 19:08:17 +0530

    Fixed background image stretching issue

--- services/bg.js
+++ services/bg.js
@@ -1,11 +1,15 @@
 (() => {
-    fetch("https://pixabay.com/api/?key=48586778-8fbbbf9581f8a1d04eab00b91&image_type=illustration&editors_choice=true&color=white",{
+    fetch("https://pixabay.com/api/?key=48586778-8fbbbf9581f8a1d04eab00b91&image_type=illustration&editors_choice=true&color=white", {
         method: "GET"
-    } ).then(res => res.json())
+    })
+    .then(res => res.json())
     .then(image => {
         console.log(image);
-        const img = image.hits[Math.floor(Math.random() * 20)].largeImageURL;
+        const img = image.hits[Math.floor(Math.random() * image.hits.length)].largeImageURL;
         const bg = document.querySelector(".mainbg");
-        bg.style.backgroundImage = `url(${img})`
-    });
+        bg.style.backgroundImage = `url(${img})`;
+        bg.style.backgroundSize = "cover"; 
+        bg.style.backgroundPosition = "center"; 
+    })
+    .catch(error => console.error("Error fetching image:", error));
 })();
